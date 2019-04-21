d1d9b050673237a05d6ab24a27334fd5b62ed385
Arman Ghazanchyan <arman-g@users.noreply.github.com>
2019-04-20 19:25:49 -0700

    Update some.component.ts

--- some.component.ts
+++ some.component.ts
@@ -1,21 +1,21 @@
- export class SomeComponent {
-     getPagination(currentPageNum, totalPageCount): Array<any> {
-        const delta = 3;
-        let pages = new Array();
-        for (let i = 0; i < totalPageCount; i++) {
-          if (i === 0 || i === totalPageCount - 1
-            || (i < currentPageNum + delta + 1 &&
-              i > currentPageNum - delta - 3)) {
-            pages.push(i + 1);
-          }
-        }
-        const currentIndex = pages.indexOf(currentPageNum);
-        if (currentIndex - this.delta > 1) {
-          pages = pages.fill("...", 1, 2);
-        }
-        if (currentIndex < pages.length - delta - 2) {
-          pages = pages.fill("...", currentIndex + delta + 1, currentIndex + delta + 2);
-        }
-        return pages;
-      }
-  }
+export class SomeComponent {
+  getPagination(currentPageNum, totalPageCount): Array<any> {
+     const delta = 3;
+     let pages = new Array();
+     for (let i = 0; i < totalPageCount; i++) {
+       if (i === 0 || i === totalPageCount - 1
+         || (i < currentPageNum + delta + 1 &&
+           i > currentPageNum - delta - 3)) {
+         pages.push(i + 1);
+       }
+     }
+     const currentIndex = pages.indexOf(currentPageNum);
+     if (currentIndex - this.delta > 1) {
+       pages = pages.fill("...", 1, 2);
+     }
+     if (currentIndex < pages.length - delta - 2) {
+       pages = pages.fill("...", currentIndex + delta + 1, currentIndex + delta + 2);
+     }
+     return pages;
+   }
+}
