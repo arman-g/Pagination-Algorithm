69187fb69967687562bdeaf7078b4b7503f21a7b
Arman Ghazanchyan <arman-g@users.noreply.github.com>
2019-04-21 22:24:09 -0700

    Update some.component.ts

--- some.component.ts
+++ some.component.ts
@@ -1,13 +1,14 @@
 export class SomeComponent {
   getPagination(currentPageNum, totalPageCount): Array<any> {
+    const delta = 2;
     const pages = new Array<any>();
     for (let i = 0; i < totalPageCount; i++) {
       const pageNum = i + 1;
       if (i === 0 || i === totalPageCount - 1) {
         pages.push(pageNum); // Add first & last pages.
       } else {
-        const left = currentPageNum - this.delta - 1;
-        const right = currentPageNum + this.delta + 1;
+        const left = currentPageNum - delta - 1;
+        const right = currentPageNum + delta + 1;
         if (i > left - 2 && i < right) {
           // Add in-between pages.
           if (left === pageNum || right === pageNum) {
