0e4dd5b604405746d0ce048d0b23760ad13bb592
Arman Ghazanchyan <arman-g@users.noreply.github.com>
2019-04-21 22:15:34 -0700

    Update some.component.ts

--- some.component.ts
+++ some.component.ts
@@ -5,11 +5,12 @@ export class SomeComponent {
     for (let i = 0; i < totalPageCount; i++) {
       const pageNum = i + 1;
       if (i === 0 || i === totalPageCount - 1) {
-        pages.push(pageNum);
+        pages.push(pageNum); // Add first & last pages.
       } else {
-        const right = currentPageNum + this.delta + 1;
         const left = currentPageNum - this.delta - 1;
-        if (i < right && i > left - 2) {
+        const right = currentPageNum + this.delta + 1;
+        if (i > left - 2 && i < right) {
+          // Add in-between pages.
           if (left === pageNum || right === pageNum) {
             pages.push("...");
           } else {
