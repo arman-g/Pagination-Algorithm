1f3a806df8d93fa7a1b58686abd03a05ea04c683
Arman Ghazanchyan <arman-g@users.noreply.github.com>
2019-04-23 21:23:55 -0700

    Update some.component.ts

--- some.component.ts
+++ some.component.ts
@@ -6,14 +6,17 @@ export class SomeComponent {
     for (let i = 0; i < totalPageCount; i++) {
       const pageNum = i + 1;
       if (i === 0 || i === totalPageCount - 1) {
+        //add first & last page numbers.
         pages.push(pageNum);
       } else {
         const left = currentPageNum - this.delta - 1;
         const right = currentPageNum + this.delta + 1;
         if (i > left - 2 && i < right) {
           if (left === pageNum || right === pageNum) {
+            //add left & right ellipsis.
             pages.push("...");
           } else {
+             //add in-between page numbers.
             pages.push(pageNum);
           }
         }
